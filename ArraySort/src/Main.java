814f8a00d24a875f3d939a9013289882cdc884c5
Yuting-H <yuting.yh@gmail.com>
2023-03-13 12:28:13 -0400

    Added random, added toString method

--- ArraySort/src/Main.java
+++ ArraySort/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * @author Yuting
  * This is the main class
@@ -7,8 +9,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		int[] arr = new int[10];
 		
+		Random rand = new Random();
 		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) (rand.nextDouble()*100);
+		}
+		
+		System.out.println(arrToString(arr));
+	
 	}
-
+	
+	public static String arrToString(int[] arr) {
+		String reString = "";
+		reString += "[";
+		for (int i = 0; i < arr.length; i++) {
+			reString += arr[i] + " ";
+		}
+		reString += "]";
+		return reString;
+	}
+	
 }
