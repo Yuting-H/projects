cfceeabd52e62db11a1eab4a68de337d75cbb01b
Yuting-H <yuting.yh@gmail.com>
2023-03-14 14:13:01 -0400

    An example of using gitignore
    note that the gitignore file is not tracked

---
+++ Temp/src/Example.java
@@ -0,0 +1,19 @@
+
+/**
+ * @author Yuting
+ * This is an example of using gitignore
+ * files related to the eclipse ide are not tracked
+ * so far, only .java files will be uploaded
+ */
+
+// make changes here
+public class Example {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Excluded files are not tracked/uploaded to github");
+	}
+
+}
