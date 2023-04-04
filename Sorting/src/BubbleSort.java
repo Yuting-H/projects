14616c71140c75becca37ff9d062e082fee3b2a6
Yuting-H <yuting.yh@gmail.com>
2023-04-04 13:24:19 -0400

    Create BubbleSort.java

---
+++ Sorting/src/BubbleSort.java
@@ -0,0 +1,44 @@
+
+/**
+ * @author Yuting
+ *
+ */
+public class BubbleSort {
+	
+	/**
+	 * Sort the array in ascending order
+	 * 
+	 * @param arr
+	 */
+	public static void sort(int[] arr) {
+		
+		int temp;
+		
+		while (!isSorted(arr)) {
+			for (int i = 0; i < arr.length -1; i++) {
+				if (arr[i] > arr[i+1]) {
+					temp = arr[i];
+					arr[i] = arr[i+1];
+					arr[i+1] = temp;
+				}
+			}
+		}
+		
+	}
+
+	/**
+	 * Determines if the arr is sorted
+	 */
+	private static boolean isSorted(int[] arr) {
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
+	
+}
