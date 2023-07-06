33b3b4b678d2941616eed348b3a71455ce76b14a
Yuting-H <yuting.yh@gmail.com>
2023-07-05 23:03:25 -0400

    Made selection sort, broken

--- Sorting/src/SelectionSort.java
+++ Sorting/src/SelectionSort.java
@@ -1,7 +1,20 @@
 /**
  * @author Yuting
+ * This is selection sort
  */
 
 public class SelectionSort {
     
+    /**
+     * 
+     * @param arr
+     */
+    public static void sort(int[] arr){
+
+        if(arr.length < 2) return;
+
+        int min;
+
+    }
+
 }
