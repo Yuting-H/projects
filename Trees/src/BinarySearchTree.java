fdd87fa53cf228574eaccb107bc2a0346d49c41d
Yuting-H <yuting.yh@gmail.com>
2023-11-20 12:23:47 -0500

    commented

--- Trees/src/BinarySearchTree.java
+++ Trees/src/BinarySearchTree.java
@@ -7,7 +7,7 @@
 public class BinarySearchTree extends BinaryTree{
 
 	/**
-	 * 
+	 * constructs a tree with root = null
 	 */
 	public BinarySearchTree() {
 		
@@ -15,20 +15,36 @@ public class BinarySearchTree extends BinaryTree{
 		
 	}
 	
+	/**
+	 * constructs a tree with root storing data
+	 * @param data
+	 */
 	public BinarySearchTree(int data) {
 	
 		super(data);
 		
 	}
 	
+	/**
+	 * driver function for adding to tree
+	 * @param data
+	 */
 	public void add(int data) {
 		
 		recursiveAdd(super.root, data);
 		
 	}
 
+	/**
+	 * recursively add to tree
+	 * @param root
+	 * @param data
+	 */
 	public void recursiveAdd(BinaryTreeNode root, int data){
-
+		
+		if (root == null) {
+			root = new BinaryTreeNode(data);
+		}
 
 		if (data <= root.getData()) {
 
